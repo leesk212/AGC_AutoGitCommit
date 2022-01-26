1ad5e968953f8676f85380ea65f30ea5d39c0ce4
leesk212 <leesk212@gmail.com>
2022-01-26 19:23:56 +0900

    AGC_tst

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo aaaaaa > test.txt
-git add .
-git commit -m "AGC_tst"
-git push
-rm test.txt
+startDate=`date +"%Y%m%d" -d "20190101"`
+
+while :
+do
+	echo $startDate > "$startDate.txt"
+	git add .
+	git commit -m "AGC_tst"
+	git push
+	rm test.txt
+done
